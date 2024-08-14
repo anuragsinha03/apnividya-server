a79a2deb85ea9fb3e3253b4356c2c1d453022f58
Anurag Sinha <anuraagrk4@gmail.com>
2024-08-14 12:13:32 +0530

    Fix: Removed GPT code

--- controller/smartNotesController.js
+++ controller/smartNotesController.js
@@ -1,38 +1,3 @@
-// import { Configuration, OpenAIApi } from "openai";
-
-// async function smartNotesController(req, res) {
-// 	//console.log(process.env.OPENAI_API);
-// 	const { inputNote } = req.body;
-// 	//console.log(inputNote);
-// 	const configuration = new Configuration({
-// 		apiKey: process.env.OPENAI_API,
-// 	});
-
-// 	//console.log(configuration);
-// 	const openai = new OpenAIApi(configuration);
-
-// 	try {
-// 		const response = await openai.chat.completions.create({
-// 			model: "text-davinci-003",
-// 			prompt: inputNote,
-// 			temperature: 0,
-// 			max_tokens: 256,
-// 			top_p: 1,
-// 			frequency_penalty: 0,
-// 			presence_penalty: 0,
-// 		});
-// 		//console.log(response.data.choices[0].text);
-// 		res.status(200).json({
-// 			summary: response.data.choices[0].text.trimStart(),
-// 		});
-// 	} catch (error) {
-// 		console.error("Error calling OpenAI API:", error);
-// 		res.status(400).json({ error });
-// 	}
-// }
-
-// export default smartNotesController;
-
 // Integrated Gemini instead of ChatGPT
 import dotenv from "dotenv";
 dotenv.config();
